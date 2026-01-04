f320f5269ae306551a260d044cd1e33d1c85426e
mhshefat99 <webdev.shefat@gmail.com>
2026-01-05 00:38:54 +0600

    Updated host

--- lib/auth-client.ts
+++ lib/auth-client.ts
@@ -1,6 +1,6 @@
 import { createAuthClient } from "better-auth/react";
 import { adminClient } from "better-auth/client/plugins";
 export const authClient = createAuthClient({
-  baseURL: "http://localhost:3000",
+  baseURL: "https://learnstack-lms.vercel.app",
   plugins: [adminClient()],
 });
